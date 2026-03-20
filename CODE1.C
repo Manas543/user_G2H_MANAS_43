54e85a9e0e953d9778c448042b65c1289667daaf
Manas543 <manaschauhan200600000@gmail.com>
2026-03-20 12:28:09 +0530

    Add files via upload

---
+++ CODE1.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a, b;
+    printf("enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    if (a==b)
+    printf("numbers are equal");
+else
+printf("numbers are not equal");
+return 0;
+}
